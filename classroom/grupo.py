f5b69e437032391be6c5e0fde8f9d1e3730d3d30
Salomé Murillo Gaviria <samurillog@unal.edu.co>
2024-03-01 17:09:06 -0500

    1
    Pequeños cambios en la clase grupo

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -3,12 +3,12 @@ from classroom.asignatura import Asignatura
 class Grupo:
     grado = None
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes):
+    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes=None):
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
 
-    def listadoAsignaturas(self, kwargs):
+    def listadoAsignaturas(self, **kwargs):
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
